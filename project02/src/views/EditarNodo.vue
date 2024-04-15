0da4e17d9987d52cfdc04465f342ffe85fcecb77
Fernanda Esquivel <80931098+FerEsq@users.noreply.github.com>
2024-04-15 16:51:06 -0600

    Edit multiples nodes at once

--- project02/src/views/EditarNodo.vue
+++ project02/src/views/EditarNodo.vue
@@ -32,6 +32,15 @@
                 />
             </div>
             <button @click.prevent="addNewProperty" class="save">Añadir propiedad</button>
+
+        <div class="form-group">
+          <label for="applyToNodes" class="changes"> * Aplicar cambios a los siguientes nodos:</label>
+          <select v-model="applyToNodes" id="applyToNodes" class="multiple-nodes">
+            <option v-for="num in 11" :key="num" :value="num - 1" >
+              {{ num - 1 }}
+            </option>
+          </select>
+        </div>
         <button type="submit" class="save">Guardar</button>
       </form>
     </div>
@@ -56,6 +65,7 @@ export default {
     return {
       nodeProperties: {}, // Aquí se almacenarán las propiedades del nodo
       newProperties: [],
+      applyToNodes: 0, // Para guardar el número seleccionado del dropdown
     };
   },
   created() {
@@ -92,68 +102,127 @@ export default {
       this.newProperties.splice(index, 1); // Elimina el elemento en el índice dado
     },
     saveChanges() {
-        const session = getSession();
-        const numericId = parseInt(this.id);
+      const session = getSession();
+      const numericId = parseInt(this.id);
 
-        // Comenzar con las propiedades existentes
-        let query = 'MATCH (n) WHERE ID(n) = $id SET ';
-        let setClauses = Object.keys(this.nodeProperties).map(key => `n.${key} = $${key}`);
-        const params = { id: numericId, ...this.nodeProperties };
+      // Comenzar con las propiedades existentes
+      let query = 'MATCH (n) WHERE ID(n) = $id SET ';
+      let setClauses = Object.keys(this.nodeProperties).map(key => `n.${key} = $${key}`);
+      const params = { id: numericId, ...this.nodeProperties };
 
-        // Añadir las nuevas propiedades
-        this.newProperties.forEach((prop, index) => {
-            if (prop.key && prop.value) { // Asegúrate de que la clave y el valor no están vacíos
-            // Agrega una nueva propiedad en la consulta Cypher y en los parámetros
-            setClauses.push(`n.${prop.key} = $newPropValue${index}`);
-            params[`newPropValue${index}`] = prop.value;
+      // Añadir las nuevas propiedades
+      this.newProperties.forEach((prop, index) => {
+          if (prop.key && prop.value) { // Asegúrate de que la clave y el valor no están vacíos
+          // Agrega una nueva propiedad en la consulta Cypher y en los parámetros
+          setClauses.push(`n.${prop.key} = $newPropValue${index}`);
+          params[`newPropValue${index}`] = prop.value;
+          }
+      });
+
+      // Completar la consulta Cypher con todas las cláusulas SET
+      query += setClauses.join(', ');
+
+      // Ejecutar la consulta Cypher con los parámetros
+      session
+          .run(query, params)
+          .then(() => {
+            if (this.applyToNodes > 0) {
+              this.applyChangesToFollowingNodes();
             }
-        });
-
-        // Completar la consulta Cypher con todas las cláusulas SET
-        query += setClauses.join(', ');
-
-        // Ejecutar la consulta Cypher con los parámetros
-        session
-            .run(query, params)
-            .then(() => {
-            // Manejar el caso de éxito
-            alert('Nodo editado con éxito!');
-            this.$router.go(-1); // Reemplaza esto con tu ruta deseada
-            })
-            .catch(error => {
+            else {
+              // Manejar el caso de éxito
+              alert('Nodo actualizado con éxito!');
+              this.$router.go(-1); // Reemplaza esto con tu ruta deseada
+            }
+            
+          })
+          .catch(error => {
             // Manejar el caso de error
             console.error('Error al guardar los cambios:', error);
-            })
-            .finally(() => {
+          })
+          .finally(() => {
             session.close(); // Cerrar la sesión
-            });
-        },
-
+          });
+    },
     deleteProperty(propertyName) {
-        // Prevenir la inyección de Cypher asegurándose de que propertyName es un nombre de propiedad válido
-        if (!propertyName.match(/^[a-zA-Z_][a-zA-Z0-9_]*$/)) {
-            console.error('Nombre de propiedad inválido para eliminación:', propertyName);
-            return;
-        }
+      // Prevenir la inyección de Cypher asegurándose de que propertyName es un nombre de propiedad válido
+      if (!propertyName.match(/^[a-zA-Z_][a-zA-Z0-9_]*$/)) {
+          console.error('Nombre de propiedad inválido para eliminación:', propertyName);
+          return;
+      }
 
-        const session = getSession();
-        const numericId = parseInt(this.id);
-        // Utiliza la interpolación de cadenas para incluir el nombre de la propiedad de forma segura.
-        const query = `MATCH (n) WHERE ID(n) = $id SET n.${propertyName} = NULL RETURN n`;
+      const session = getSession();
+      const numericId = parseInt(this.id);
+      // Utiliza la interpolación de cadenas para incluir el nombre de la propiedad de forma segura.
+      const query = `MATCH (n) WHERE ID(n) = $id SET n.${propertyName} = NULL RETURN n`;
 
-        session
-            .run(query, { id: numericId })
-            .then(() => {
-            // Elimina la propiedad del objeto local después de una eliminación exitosa
-            delete this.nodeProperties[propertyName];
-            })
-            .catch(error => {
-            console.error('Error al eliminar la propiedad:', error);
-            })
-            .finally(() => {
+      session
+        .run(query, { id: numericId })
+          .then(() => {
+          // Elimina la propiedad del objeto local después de una eliminación exitosa
+          delete this.nodeProperties[propertyName];
+          alert('Nodos actualizados con éxito!');
+        })
+        .catch(error => {
+          console.error('Error al eliminar la propiedad:', error);
+        })
+        .finally(() => {
+          if (this.applyToNodes <= 0) {
             session.close();
-            });
-        },
+          }
+        });
+    },
+    applyChangesToFollowingNodes() {
+      const session = getSession();
+      const numericId = parseInt(this.id);
+      const startId = numericId;
+      const endId = startId + this.applyToNodes;
+      // Primero, obtenemos las etiquetas del nodo original
+      session.run('MATCH (n) WHERE ID(n) = $id RETURN labels(n) as labels', { id: numericId })
+        .then(result => {
+          if (result.records.length == this.applyToNodes) {
+            const originalNodeLabels = result.records[0].get('labels');
+            // Ahora aplicamos los cambios a los nodos sucesivos que compartan al menos una etiqueta
+            for (let i = 1; i <= this.applyToNodes; i++) {
+              let nodeId = startId + i;
+              // Verificamos si el nodo sucesivo comparte alguna etiqueta
+              session.run(`
+                MATCH (n)
+                WHERE ID(n) = $nodeId AND any(label IN labels(n) WHERE label IN $originalNodeLabels)
+                SET n += $properties
+                RETURN n
+              `, {
+                nodeId: nodeId,
+                originalNodeLabels: originalNodeLabels,
+                properties: this.nodeProperties
+              })
+              .then(result => {
+                if (result.records.length === 0) {
+                  alert('ERROR: Alguno de los nodos a editar no comparte etiquetas con el nodo original.');
+                } else {
+                  alert(`Nodos actualizados con éxito!`);
+                  this.$router.go(-1); // Reemplaza esto con tu ruta deseada
+                }
+              })
+              .catch(error => {
+                console.error(`Error al actualizar el nodo con ID ${nodeId}:`, error);
+              })
+              .finally(() => {
+                if (nodeId === endId) {
+                  session.close(); // Asegurarnos de cerrar la sesión cuando hayamos terminado con el último nodo
+                }
+              });
+            }
+          } else {
+            alert('ERROR: Alguno de los nodos a editar no comparte etiquetas con el nodo original.');
+            session.close();
+          }
+        })
+        .catch(error => {
+          console.error('Error al recuperar las etiquetas del nodo original:', error);
+          session.close();
+        });
+    },
   },
 };
 </script>
@@ -186,6 +255,15 @@ export default {
   margin-bottom: 2%;
 }
 
+.changes {
+  border-collapse: collapse;
+  font-family: Verdana, Geneva, sans-serif;
+  font-size: 16px;
+  margin-left: 1%;
+  font-weight: 900;
+  color: #226946;
+}
+
 .property{
   display: block !important;
   font-family: Verdana, Geneva, sans-serif;
@@ -197,6 +275,16 @@ export default {
 .form-control {
   display: inline-block !important;
   width: 94% !important;
+  margin-left: 1% !important;
+  margin: 1rem 0;
+  border: 2px solid #226946 !important;
+  border-radius: 4px;
+}
+
+.multiple-nodes {
+  display: inline-block !important;
+  width: 20% !important;
+  height: 30px !important;
   margin-left: 1% !important;
   margin: 1rem 0;
   border: 2px solid #226946 !important;
