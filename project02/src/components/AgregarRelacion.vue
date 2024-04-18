fcd0c57d117ff40a85e0d76178f51a8321d76973
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2024-04-17 23:45:47 -0600

    Update AgregarRelacion.vue

--- project02/src/components/AgregarRelacion.vue
+++ project02/src/components/AgregarRelacion.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <h1>Crear nueva relación</h1>
+      <h1 v-if="relacionInicial">Crear nueva relación</h1>
       <form @submit.prevent="crearRelacion">
         <label>
           Nodo de origen:
@@ -8,26 +8,31 @@
         </label>
         <label>
           Etiqueta Nodo destino:
-          <select v-model="etiquetaNodoDestino" @change="obtenerNodosDestino">
+          <select v-model="etiquetaNodoDestino" @change="obtenerNodosDestino" :disabled="relacionInicial">
             <option v-for="etiqueta in etiquetas" :key="etiqueta" :value="etiqueta">{{ etiqueta }}</option>
           </select>
         </label>
         <label>
           Nodo destino:
-          <select v-model="nodoDestino">
+          <select v-model="nodoDestino" :disabled="relacionInicial">
             <option v-for="nodo in nodosDestino" :key="nodo.id" :value="nodo.identity.low">{{ nodo.identity.low }}</option>
           </select>
         </label>
         <label>
           Tipo de relación:
-          <select v-model="tipoRelacion">
+          <select v-model="tipoRelacion" :disabled="relacionInicial">
             <option v-for="relacion in relaciones" :key="relacion" :value="relacion">{{ relacion }}</option>
           </select>
         </label>
-        <div v-for="(valor, nombre, index) in propiedades" :key="nombre" class="props">
+        <div v-for="(valor, nombre) in propiedades" :key="nombre" class="props">
             <label>
-                <input type="text" v-model="Object.keys(propiedades)[index]" required>
+                {{ nombre }}:
                 <input :type="obtenerTipoInput(valor)" v-model="propiedades[nombre]" step="any" required>
+                <button @click.prevent="deleteProperty(nombre)">
+                  <svg class="trash" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+                    <path d="M14 10V17M10 10V17M6 6V17.8C6 18.9201 6 19.4798 6.21799 19.9076C6.40973 20.2839 6.71547 20.5905 7.0918 20.7822C7.5192 21 8.07899 21 9.19691 21H14.8031C15.921 21 16.48 21 16.9074 20.7822C17.2837 20.5905 17.5905 20.2839 17.7822 19.9076C18 19.4802 18 18.921 18 17.8031V6M6 6H8M6 6H4M8 6H16M8 6C8 5.06812 8 4.60241 8.15224 4.23486C8.35523 3.74481 8.74432 3.35523 9.23438 3.15224C9.60192 3 10.0681 3 11 3H13C13.9319 3 14.3978 3 14.7654 3.15224C15.2554 3.35523 15.6447 3.74481 15.8477 4.23486C15.9999 4.6024 16 5.06812 16 6M16 6H18M18 6H20" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>
+                </button>
             </label>
         </div>
         <div v-for="(propiedad, index) in nuevasPropiedades" :key="index">
@@ -172,60 +177,122 @@ export default {
           session.close();
         });
     },
+    deleteProperty(nombre) {
+      console.log('Eliminar propiedad:', nombre);
+      if (confirm('¿Estás seguro de que deseas eliminar esta propiedad?')) {
+        const session = getSession();
+        const partesElementId = this.relacionInicial.relation.elementId.split(':');
+        const idRelacion = partesElementId[2];
+
+        const query = `MATCH ()-[r]->() WHERE ID(r) = toInteger($idRelation) REMOVE r.${nombre}`;
+        const params = { idRelation: idRelacion };
+
+        session.run(query, params)
+          .then(() => {
+            alert('Propiedad eliminada con éxito!');
+            location.reload();
+          })
+          .catch((error) => {
+            console.error('Error al eliminar la propiedad:', error);
+            alert('No se pudo eliminar la propiedad.');
+          });
+      }
+    },
     crearRelacion() {
-    if (!this.nodoOrigen || !this.nodoDestino) {
-        alert('Por favor, selecciona los nodos de origen y destino.');
-        return;
-    }
+      const session = getSession();
+      let query = ''
+      let params = {};
 
-    if (!this.tipoRelacion) {
-        alert('Por favor, selecciona el tipo de relación.');
-        return;
-    }
+      if (this.relacionInicial) {
+        // Estamos editando una relación existente
+        const partesElementId = this.relacionInicial.relation.elementId.split(':');
+        const idRelacion = partesElementId[2];
+        query = `MATCH ()-[r]->() WHERE ID(r) = toInteger($idRelation) SET `;
+        /////////////
+        let setClauses = Object.keys(this.propiedades).map(key => `r.${key} = $${key}`);
+        params = { idRelation: idRelacion, ...this.propiedades };
 
-    if (this.nuevasPropiedades.length < 3) {
-        alert('Por favor, agregar mínimo 3 propiedades.');
-        return;
-    }
-
-    const session = getSession();
-    let propertiesObject = { ...this.propiedades };
-    this.nuevasPropiedades.forEach((prop) => {
-        if (prop.key.trim() && prop.value.trim()) {
-        propertiesObject[prop.key.trim()] = prop.value.trim();
-        }
-    });
-
-    const query = `
-        MATCH (a),(b)
-        WHERE ID(a) = toInteger($nodoOrigen) AND ID(b) = toInteger($nodoDestino)
-        CREATE (a)-[r:${this.tipoRelacion} $properties]->(b)
-        RETURN r
-    `;
-    const params = {
-        nodoOrigen: this.nodoOrigen,
-        nodoDestino: this.nodoDestino,
-        properties: propertiesObject,
-    };
-
-    session.run(query, params)
-        .then((result) => {
-        if (result.records.length > 0) {
-            const createdRelation = result.records[0].get('r');
-            console.log('Relación creada con éxito:', createdRelation);
-            alert('Relación creada con éxito!');
-            // location.reload();
-        } else {
-            alert('No se pudo crear la relación.');
-        }
-        })
-        .catch((error) => {
-        console.error('Error al crear la relación:', error);
-        alert('Hubo un error al crear la relación.');
-        })
-        .finally(() => {
-        session.close();
+        // Añadir las nuevas propiedades
+        this.nuevasPropiedades.forEach((prop, index) => {
+            if (prop.key && prop.value) { // Asegúrate de que la clave y el valor no están vacíos
+            // Agrega una nueva propiedad en la consulta Cypher y en los parámetros
+            setClauses.push(`r.${prop.key} = $newPropValue${index}`);
+            params[`newPropValue${index}`] = prop.value;
+            }
         });
+
+        // Completar la consulta Cypher con todas las cláusulas SET
+        query += setClauses.join(', ');
+
+
+        /* console.log('Query:', query);
+        console.log('Params:', params);
+        console.log('Propiedades nuevas:', this.nuevasPropiedades); */
+
+        session.run(query, params)
+          .then(() => {
+            alert('Relación actualizada con éxito!');
+            location.reload();
+          })
+          .catch((error) => {
+            console.error('Error al actualizar la relación:', error);
+            alert('No se pudo actualizar la relación.');
+          });
+        } else { // crear nueva relacion
+          if (!this.nodoOrigen || !this.nodoDestino) {
+              alert('Por favor, selecciona los nodos de origen y destino.');
+              return;
+          }
+
+          if (!this.tipoRelacion) {
+              alert('Por favor, selecciona el tipo de relación.');
+              return;
+          }
+
+          if (this.nuevasPropiedades.length < 3) {
+              alert('Por favor, agregar mínimo 3 propiedades.');
+              return;
+          }
+
+          
+          let propertiesObject = { ...this.propiedades };
+          this.nuevasPropiedades.forEach((prop) => {
+              if (prop.key.trim() && prop.value.trim()) {
+              propertiesObject[prop.key.trim()] = prop.value.trim();
+              }
+          });
+
+          query = `
+              MATCH (a),(b)
+              WHERE ID(a) = toInteger($nodoOrigen) AND ID(b) = toInteger($nodoDestino)
+              CREATE (a)-[r:${this.tipoRelacion} $properties]->(b)
+              RETURN r
+          `;
+          params = {
+              nodoOrigen: this.nodoOrigen,
+              nodoDestino: this.nodoDestino,
+              properties: propertiesObject,
+          };
+
+          session.run(query, params)
+              .then((result) => {
+              if (result.records.length > 0) {
+                  const createdRelation = result.records[0].get('r');
+                  console.log('Relación creada con éxito:', createdRelation);
+                  alert('Relación creada con éxito!');
+                  location.reload();
+              } else {
+                  alert('No se pudo crear la relación.');
+              }
+              })
+              .catch((error) => {
+              console.error('Error al crear la relación:', error);
+              alert('Hubo un error al crear la relación.');
+              })
+              .finally(() => {
+              session.close();
+              });
+          }
     },
   },
 };
